c1cab747fa8c93f787d0386f29ccfe1fd111002a
Sean Breckenridge <seanbrecke@gmail.com>
2023-08-22 21:58:48 -0700

    spelling fix

--- chess_export/common.py
+++ chess_export/common.py
@@ -23,7 +23,7 @@ def safe_request(
     eprint(f"Requesting {url}")
     resp: requests.Response = requests.get(url, **kwargs)
     if resp.status_code == 429:
-        eprint("Error: Recieved 429 error, waiting...")
+        eprint("Error: Received 429 error, waiting...")
         time.sleep(backoff_time or BACKOFF_TIME)
         # recursive call
         kwargs["backoff_time"] = backoff_time
@@ -31,7 +31,7 @@ def safe_request(
         return safe_request(url, **kwargs)
     else:
         if resp.status_code != 200:
-            eprint(f"Warning: {url} recieved non 200 exit code: {resp.status_code}")
+            eprint(f"Warning: {url} received non 200 exit code: {resp.status_code}")
         time.sleep(sleep_time or SLEEP_TIME)
         return resp
 
