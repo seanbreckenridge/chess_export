394e57d505cd94483b36cee4a74626fdb09e266b
Sean Breckenridge <seanbrecke@gmail.com>
2023-08-27 14:03:28 -0700

    print more debug info about chessdotcom errors

--- chess_export/chessdotcom/export.py
+++ chess_export/chessdotcom/export.py
@@ -4,7 +4,7 @@ Request/Export your games from chess.com
 
 from typing import Iterator, List
 
-from ..common import Json, safe_request
+from ..common import Json, safe_request_json
 
 BASE_URL = "https://api.chess.com/pub/"
 
@@ -12,11 +12,11 @@ BASE_URL = "https://api.chess.com/pub/"
 def get_player_game_archives(username: str) -> List[str]:
     """Returns a list of monthly archive URLs for the user"""
     url = BASE_URL + "/".join(("player", username, "games", "archives"))
-    mresp = safe_request(url).json()
+    mresp = safe_request_json(url)
     return list(mresp["archives"])
 
 
 def get_player_games(username: str) -> Iterator[Json]:
     """Returns all accessible games, using the monthly archive as the source"""
     for archive_url in get_player_game_archives(username):
-        yield from safe_request(archive_url).json()["games"]
+        yield from safe_request_json(archive_url)["games"]
--- chess_export/common.py
+++ chess_export/common.py
@@ -1,4 +1,5 @@
 import time
+import json
 from typing import Any, Optional
 from functools import partial
 from enum import Enum
@@ -22,8 +23,13 @@ def safe_request(
 ) -> requests.Response:
     eprint(f"Requesting {url}")
     resp: requests.Response = requests.get(url, **kwargs)
-    if resp.status_code == 429:
-        eprint("Error: Received 429 error, waiting...")
+    # https://www.chess.com/clubs/forum/view/error-403-when-accessed-from-php
+    # sometimes it seems this fails with a 403 when the servers are too overloaded?
+    # has a title that says 'Just a moment...'
+    if resp.status_code > 400 and resp.status_code != 404:
+        eprint(f"Error: Received {resp.status_code} error, waiting...")
+        eprint(f"Text: {resp.text}")
+        eprint(f"Headers: {json.dumps(dict(resp.headers), indent=2)}")
         time.sleep(backoff_time or BACKOFF_TIME)
         # recursive call
         kwargs["backoff_time"] = backoff_time
@@ -34,6 +40,23 @@ def safe_request(
             eprint(f"Warning: {url} received non 200 exit code: {resp.status_code}")
         time.sleep(sleep_time or SLEEP_TIME)
         return resp
+
+
+def safe_request_json(
+    url: str,
+    sleep_time: Optional[float] = None,
+    backoff_time: Optional[float] = None,
+    **kwargs: Any,
+) -> Json:
+    req: Optional[requests.Response] = None
+    try:
+        req = safe_request(url, sleep_time, backoff_time, **kwargs)
+        return req.json()
+    except requests.exceptions.JSONDecodeError as e:
+        eprint(f"Warning: {url} received non-JSON response, retrying...")
+        if req:
+            eprint(req.text)
+            raise e
 
 
 class Result(Enum):
